ec3da41c454890461886f52a5a5b3f81a687f2c5
Alexey Vasilyev <alexa.infra@gmail.com>
2018-04-20 16:27:53 +0200

    restructure

--- server/api/index.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const links = require('./links')
-
-router.use('/links', links)
-
-module.exports = router
--- server/main.js
+++
@@ -1,9 +0,0 @@
-const makeApp = require('./app').makeApp
-const config = require('./config')
-const app = makeApp(config)
-
-const port = process.env.PORT || 8000
-
-app.listen(port, () => {
-  console.log('Example app listening on port.. ' + port)
-})
