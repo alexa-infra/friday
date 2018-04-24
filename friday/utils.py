9327ff65a305351f37f431cae9d15830d1939c11
Alexey Vasilyev <alexa.infra@gmail.com>
2018-04-24 14:57:18 +0200

    added bcrypt dependency and password hashing utils

--- friday/utils.py
+++ friday/utils.py
@@ -1,5 +1,15 @@
 from datetime import datetime
+import bcrypt
 
 
 def utcnow():
     return datetime.utcnow()
+
+def make_password_hash(password):
+    salt = bcrypt.gensalt(prefix=b"2b", rounds=12)
+    hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
+    return hashed_password.decode('utf-8')
+
+def check_password_hash(password, hashed_password):
+    hashed = hashed_password.encode('utf-8')
+    return bcrypt.hashpw(password.encode('utf-8'), hashed) == hashed
