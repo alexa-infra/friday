bb40a6f83d20ea0f4ca7649a92b14a448ab09f7e
Alexey Vasilyev <alexa.infra@gmail.com>
2018-04-28 08:52:02 +0200

    remove api middleware, change it to a function

--- friday-ui/src/store/index.js
+++ friday-ui/src/store/index.js
@@ -3,12 +3,10 @@ import { routerMiddleware } from 'react-router-redux';
 import { logger } from 'redux-logger';
 import thunk from 'redux-thunk';
 import reducers from '../reducers';
-import { apiErrorMiddleware } from '../api';
 
 const configureStore = (history, initialState = {}) => {
   const middleware = applyMiddleware(routerMiddleware(history),
                                      thunk,
-                                     apiErrorMiddleware,
                                      logger);
   const makeStore = compose(middleware)(createStore);
   const store = makeStore(reducers, initialState);
