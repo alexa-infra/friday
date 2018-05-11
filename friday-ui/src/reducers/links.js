b1bed39f660498418cb03752972c39382ed002d7
Alexey Vasilyev <alexa.infra@gmail.com>
2018-05-11 15:27:08 +0200

    filter links

--- friday-ui/src/reducers/links.js
+++ friday-ui/src/reducers/links.js
@@ -3,12 +3,26 @@ import { Actions } from '../constants';
 const initialState = {
   currentItem: null,
   items: [],
+  allItems: [],
+  filter: '',
 };
+
+const filterItems = term => item => {
+  if (!item.title)
+    return false;
+  const text = item.title.replace(/\s+/g, ' ').toLowerCase();
+  return ~text.indexOf(term);
+}
 
 export default function (state = initialState, action) {
   switch (action.type) {
     case Actions.LINKS_SUCCESS:
-      return { currentItem: null, items: action.data };
+      return { ...state, currentItem: null, items: action.data, allItems: action.data, filter: '' };
+    case Actions.LINKS_FILTER: {
+      const query = action.data;
+      const filtered = state.allItems.filter(filterItems(query));
+      return { ...state, items: filtered, filter: query };
+    }
     default:
       return state;
   }
