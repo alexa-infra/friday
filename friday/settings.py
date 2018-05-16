cb963d9b7c2a2b60757e0609b4f6b9c8405b4338
Alexey Vasilyev <alexa.infra@gmail.com>
2018-05-16 16:45:33 +0200

    jwt token expiration - 90days

--- friday/settings.py
+++ friday/settings.py
@@ -1,4 +1,5 @@
 import os
+from datetime import timedelta
 
 this_dir = os.path.dirname(os.path.abspath(__file__))
 
@@ -11,3 +12,5 @@ SQLALCHEMY_ECHO = True
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 SERVER_NAME = os.environ.get('SERVER_NAME', None)
+
+JWT_ACCESS_TOKEN_EXPIRES = timedelta(days=90)
