656eada945b569e452fa43eafb005d9012a7330a
Alexey Vasilyev <alexa.infra@gmail.com>
2018-05-23 08:29:47 +0200

    bookmark slug and search by slug

--- friday/models/bookmark.py
+++ friday/models/bookmark.py
@@ -1,4 +1,6 @@
+from urllib.parse import unquote
 from sqlalchemy import Column, Integer, Text, DateTime, Boolean
+from slugify import slugify
 from friday.utils import utcnow
 from . import db
 
@@ -11,12 +13,35 @@ class Bookmark(db.Model):
     updated = Column(DateTime, nullable=False, default=utcnow,
                      onupdate=utcnow)
     readed = Column(Boolean, nullable=False, default=False)
+    slug = Column(Text)
 
     @classmethod
     def new(cls, **kwargs):
         obj = cls(**kwargs)
+        obj.update_slug()
         return obj
+
+    @property
+    def unquoted_url(self):
+        return unquote(self.url)
+
+    @property
+    def clean_url(self):
+        return self.unquoted_url.strip('http://').strip('https://')
+
+    @property
+    def _slug(self):
+        txt = ' '.join(filter(None, [
+            self.clean_url,
+            self.title,
+        ]))
+        return slugify(txt, separator=' ')
+
+    def update_slug(self):
+        self.slug = self._slug
 
     def update(self, **kwargs):
         for k, v in kwargs.items():
             setattr(self, k, v)
+        if 'url' in kwargs or 'title' in kwargs:
+            self.update_slug()
--- friday/views/bookmark.py
+++ friday/views/bookmark.py
@@ -1,6 +1,7 @@
 from webargs import fields
 from webargs.flaskparser import use_args, use_kwargs
 from flask_jwt_extended import jwt_required
+from slugify import slugify
 from . import api, BaseView
 from ..models import db
 from ..models import Bookmark as BookmarkModel
@@ -8,16 +9,25 @@ from ..schemas import Bookmark as BookmarkSchema
 from .link import pagination_args
 
 
+filter_args = {
+    'search': fields.Str(location='query'),
+}
+
 class BookmarkListView(BaseView):
     route_base = '/bookmarks'
     decorators = (jwt_required,)
 
     @use_kwargs(pagination_args)
-    def get(self, page, per_page):
-        pagination = (
-            BookmarkModel.query
-            .order_by(BookmarkModel.created.desc())
-            .paginate(page, per_page))
+    @use_kwargs(filter_args)
+    def get(self, page, per_page, search):
+        query = BookmarkModel.query
+        search = slugify(search, separator=' ') if search else None
+        if search:
+            search_term = '%{}%'.format(search)
+            query = query.filter(
+                BookmarkModel.title.like(search_term))
+        query = query.order_by(BookmarkModel.created.desc())
+        pagination = query.paginate(page, per_page)
         return BookmarkSchema.jsonify(pagination), 200
 
     @use_args(BookmarkSchema())
