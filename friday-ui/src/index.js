981665e1807f3af2db1fb740cbbed2f2aecb7fdc
Alexey Vasilyev <alexa.infra@gmail.com>
2018-10-15 17:38:43 +0200

    refactoring css, use twitter bootstrap

--- friday-ui/src/index.js
+++ friday-ui/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.css'
 import { Root } from './containers';
 import configureStore, { history } from './store';
 import registerServiceWorker from './registerServiceWorker';
