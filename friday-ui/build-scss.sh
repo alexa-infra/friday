5d27eb1de5890b5f51b4c34e0dbf66d26f7e17cf
Alexey Vasilyev <alexa.infra@gmail.com>
2018-10-15 12:41:13 +0200

    build css commands

--- friday-ui/build-scss.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-SASS_BIN="./node_modules/.bin/node-sass"
-SASS_ARGS="--output-style expanded"
-
-find src/ -name "*.scss" | \
-  sed "s/.scss$//g" | \
-  xargs --replace bash -c "echo {}.scss && $SASS_BIN $SASS_ARGS {}.scss > {}.css"
