ad3f9f829e506905a57b9a2f07363249bcea429b
Alexey Vasilyev <alexa.infra@gmail.com>
2018-11-02 14:05:12 +0100

    refactor root page structure

--- friday-ui/src/pages/index.js
+++ friday-ui/src/pages/index.js
@@ -3,3 +3,4 @@ export { default as Login } from './login';
 export { default as Events } from './events';
 export { default as Links } from './links';
 export { default as Bookmarks } from './bookmarks';
+export { default as Root } from './root';
---
+++ friday-ui/src/pages/root/index.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import { Provider } from 'react-redux'
+import { Router, Route, Switch } from 'react-router'
+import './root.css'
+
+import Docs from '../docs';
+import Login from '../login';
+import Events from '../events';
+import Links from '../links';
+import Bookmarks from '../bookmarks';
+
+import { NavBar, Alerts } from '../../components'
+
+
+const Root = ({store, history}) => (
+  <Provider store={store}>
+    <Router history={history}>
+      <content className="theme-l5">
+        <header>
+          <NavBar />
+        </header>
+        <main>
+          <Alerts />
+          <Switch>
+            <Route path="/login" component={Login} />
+            <Route path="/events" component={Events} />
+            <Route path="/bookmarks" component={Bookmarks} />
+            <Route path="/docs" component={Docs} />
+            <Route path="/" component={Links} />
+          </Switch>
+        </main>
+      </content>
+    </Router>
+  </Provider>
+);
+
+export default Root;
