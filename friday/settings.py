a70ec6979cf2e148d0a71ccc0c69a61b269d469f
Alexey Vasilyev <alexa.infra@gmail.com>
2018-12-06 13:38:07 +0100

    minor changes in configs

--- friday/settings.py
+++ friday/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = 'blah-blah-blah'
 
 db_path = os.path.join(this_dir, 'database.sqlite')
 SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(db_path)
-SQLALCHEMY_ECHO = True
+#SQLALCHEMY_ECHO = True
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 SERVER_NAME = os.environ.get('SERVER_NAME', None)
