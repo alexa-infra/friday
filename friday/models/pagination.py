a554f67ab85c3ffec0d556ed2743fa1b525ae554
Alexey Vasilyev <alexey@alexadotlife.com>
2020-10-18 11:50:40 +0200

    fix pagination total count

--- friday/models/pagination.py
+++ friday/models/pagination.py
@@ -1,14 +1,21 @@
-from math import ceil
+import math
+import sqlalchemy as sa
 
 
 def paginate(query, page, per_page):
+    session = query.session
+    total = (
+        session.query(sa.func.count())
+        .select_from(
+            query.order_by(None).subquery()
+        )
+        .scalar()
+    )
+
+    if (page - 1) * per_page >= total:
+        page = 1
 
     items = query.limit(per_page).offset((page - 1) * per_page).all()
-
-    if page == 1 and len(items) < per_page:
-        total = len(items)
-    else:
-        total = query.order_by(None).count()
 
     return Pagination(query, page, per_page, total, items)
 
@@ -27,7 +34,7 @@ class Pagination:
         if self.per_page == 0:
             pages = 0
         else:
-            pages = int(ceil(self.total / float(self.per_page)))
+            pages = int(math.ceil(self.total / float(self.per_page)))
         return pages
 
     def prev(self):
