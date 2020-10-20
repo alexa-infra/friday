611eaa999fc48b25aee9841bb89af66a4e71efd8
Alexey Vasilyev <alexey@alexadotlife.com>
2020-10-20 06:27:02 +0200

    disable debugger and reloader

--- friday/app.py
+++ friday/app.py
@@ -6,4 +6,4 @@ app = make_app()
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(use_debugger=False, use_reloader=False, host="0.0.0.0", port=port)
